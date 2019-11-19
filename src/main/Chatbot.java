2fffd2e9e93428c9f5094aad7d2887765bd9e070
HIROYA CHINEN <e195738@ie.u-ryukyu.ac.jp>
2019-11-19 11:06:21 +0900

    2_4 had finished

--- src/main/Chatbot.java
+++ src/main/Chatbot.java
@@ -1,2 +1,13 @@
 public class Chatbot {
+    String name;
+
+    //コンストラクタ生成部
+    public Chatbot() {
+        System.out.println("チャットボットの原型を作成します。");
+
+    }
+    public Chatbot(String str) {
+        this.name = str;
+        System.out.println("チャットボット名" + name + "を作成します。");
+    }
 }
--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String args[]) {
         System.out.println("main method runs");
+
+        Chatbot bot1 = new Chatbot();
+
+        Chatbot bot2 = new Chatbot("自由人");
     }
 }
